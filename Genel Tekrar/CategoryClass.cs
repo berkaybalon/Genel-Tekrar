08622ef2e5dbc644eda0d02caa45a3fd2de4f1c5
Bemar <Bemar@DESKTOP-5JOPI67>
2022-05-29 14:52:31 +0300

    .

---
+++ Genel Tekrar/CategoryClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genel_Tekrar
+{
+    public class Category
+    {
+
+        public string CategoryName { get; set; }
+        public int CategoryId { get; set; }
+
+
+
+
+    }
+}
---
+++ Genel Tekrar/ProductClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genel_Tekrar
+{
+    
+
+    public class Product
+    {
+
+        public string ProductName { get; set; }
+        public int ProductId { get; set; }
+        public int ProductStock { get; set; }
+        public int ProductUnitPrace { get; set; }
+
+        
+
+
+    }
+
+
+
+}
